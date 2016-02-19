a3f562d84f7834040db42e83a31260906c7c258f
jpaldi91 <jpaldi91@outlook.com>
2016-02-18 22:15:09 -0200

    fixed Windows build

--- main.cpp
+++ main.cpp
@@ -6,16 +6,12 @@ and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
 #if defined(__APPLE__)
-#ifdef GL_ES_VERSION_2_0
-#    include <OpenGLES/ES2/gl.h>
-#  else
-#    include <OpenGL/gl.h>
+#  ifdef GL_ES_VERSION_2_0
 #  endif
+#  error "OSX unsuported"
 #elif defined(_WIN32) || defined(_WIN64)
-#  include <GL/wglew.h>
 #  include <SDL.h>
 #else
-#  include <GL/gl.h>
 #  include <SDL2/SDL.h>
 #endif
 
